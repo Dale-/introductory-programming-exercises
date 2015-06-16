0e2b9d34b1b7ea12bccfd9102110acb4b3d866fb
Dale <18717394542@163.com>
2015-06-16 23:04:04 +0800

    create one function of isosceles_triangle in Diamond

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,12 +1,22 @@
 
 public class Diamond {
 
-//    public String isosceles_triangle(int n) {
-//        String triangle = "";
-//        for(int i = 0; i < n; i++) {
-//            for(int j = 0; j < n - i; j++) {
-//                triangle += " "
-//            }
-//        }
-//    }
+    public String isosceles_triangle(int n) {
+        String triangle = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i ; j++) {
+                triangle += " ";
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        Diamond diamond = new Diamond();
+        System.out.println(diamond.isosceles_triangle(3));
+    }
 }
