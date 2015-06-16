0dafdfa43ac723fe19f1543dea7815ab3a13f96e
Dale <18717394542@163.com>
2015-06-17 00:44:26 +0800

    create one function of fizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,15 +1,27 @@
 
 public class FizzBuzz {
 
-    public String fizzBuzz () {
+    public String fizzBuzz (int n) {
         String output = "";
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 3 == 0) {
                 if (i % 5 == 0 ) {
-                    
+                    output += "FizzBuzz\n";
+                } else {
+                    output += "Fizz\n";
                 }
+            } else if (i % 5 == 0) {
+                output += "Buzz\n";
+            } else {
+                output += i + "\n";
             }
         }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(15));
     }
 
 }
