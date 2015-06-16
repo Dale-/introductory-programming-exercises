388b3019bf3f6449880d6c0b844196b1d308ffd7
Dale <18717394542@163.com>
2015-06-17 01:00:36 +0800

    create the new function of generate in order to generating factors

--- src/Factors.java
+++ src/Factors.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Factors {
+
+    public ArrayList<Integer> generate(int n) {
+        ArrayList<Integer> factorArrayList = new ArrayList<Integer>();
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                factorArrayList.add(i);
+            }
+        }
+        return factorArrayList;
+    }
+
+    
 }
