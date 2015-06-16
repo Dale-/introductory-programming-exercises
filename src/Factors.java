33ad7818034d97ab0e272fcb76c0955cb58d3631
Dale <18717394542@163.com>
2015-06-17 01:27:33 +0800

    create the test of TriangleTest

--- src/Factors.java
+++ src/Factors.java
@@ -4,13 +4,23 @@ public class Factors {
 
     public ArrayList<Integer> generate(int n) {
         ArrayList<Integer> factorArrayList = new ArrayList<Integer>();
-        for (int i = 2; i < n; i++) {
-            if(n % i == 0) {
-                factorArrayList.add(i);
-            }
+        for (int i = 2; i <= n; i++) {
+           while (n % i == 0) {
+               factorArrayList.add(i);
+               n = n / i;
+           }
         }
         return factorArrayList;
     }
 
-    
+    public static void main(String[] args) {
+        Factors factors = new Factors();
+        String output = "";
+        ArrayList<Integer> factorArrayList = factors.generate(30);
+        for (int i: factorArrayList) {
+            output += i + ",";
+        }
+        System.out.println(output.substring(0, output.length() - 1));
+    }
+
 }
--- test/TriangleTest.java
+++ test/TriangleTest.java
@@ -1,14 +1,37 @@
 import java.lang.Exception;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class TriangleTest {
 
+    private Triangle triangle;
+
+    @Before
+    public void setUp() throws Exception {
+        triangle = new Triangle();
+    }
+
     @Test
     public void testTriangle() throws Exception {
-        Triangle triangle = new Triangle();
         assertEquals("*", triangle.easy(3));
     }
 
+    @Test
+    public void testHorizontal() throws Exception {
+        assertEquals("******", triangle.horizontal(6));
+    }
+
+    @Test
+    public void testVertical() throws Exception {
+        assertEquals("*\n" + "*\n", triangle.vertical(2));
+    }
+
+    @Test
+    public void testRight() throws Exception {
+        assertEquals("*\n" + "**\n" + "***\n", triangle.rightTriangle(3));
+
+
+    }
 }
